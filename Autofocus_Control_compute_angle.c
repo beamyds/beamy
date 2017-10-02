30706095c571ba3d7f188de55870443b92308c13
dpicard <damien.picard@3ds.com>
2017-10-02 18:05:15 +0200

    [GDE] Modification of runnable compute_angle

--- Autofocus_Control_compute_angle.c
+++ Autofocus_Control_compute_angle.c
@@ -6,10 +6,10 @@
 #define C0 213.08
 
 void compute_angle(void){
-	uint16_t* distance_to_screen;
+	uint16_t distance_to_screen = 0;
    	float motor_angle = 0;
   
-  	distance_to_screen = RTE_Read_screen_distance();
+  	distance_to_screen = *RTE_Read_screen_distance();
   
   	if(distance_to_screen > DISTANCE_MIN && distance_to_screen < DISTANCE_MAX){
     	motor_angle = C2 * distance_to_screen * distance_to_screen + C1 * distance_to_screen + C0;
