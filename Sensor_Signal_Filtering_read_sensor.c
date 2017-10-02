d087f2d2065e4ac561ac675bdd48e8a535cc91e7
dpicard <damien.picard@3ds.com>
2017-10-02 18:06:12 +0200

    [GDE] Modification of runnable read_sensor

--- Sensor_Signal_Filtering_read_sensor.c
+++ Sensor_Signal_Filtering_read_sensor.c
@@ -5,7 +5,7 @@ void read_sensor(void){
 	uint16_t new_distance = 0;
   	static uint16_t distance = 0;
   
-	RTE_Call_read_distance(&new_distance);
+	new_distance = *RTE_Call_read_distance();
   	// Remove noise
   	distance = K * new_distance + (1 - K) * distance;
   	
