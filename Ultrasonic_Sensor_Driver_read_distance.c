736837079cab8b2dc7230ef1574f6730e96d4d03
dpicard <damien.picard@3ds.com>
2017-10-05 14:42:58 +0200

    [GDE] Modification of runnable read_distance

--- Ultrasonic_Sensor_Driver_read_distance.c
+++ Ultrasonic_Sensor_Driver_read_distance.c
@@ -1,4 +1,7 @@
-void read_distance(uint16_t* distance){
-	// FIXME read a distance on sensor
-  (void) distance;
+void read_distance(int* distance){
+  fmi2Integer data;
+  
+  fmi2IntegerReadFromWriteBuffer(InputId_Switch_Button_Driver_read_switch_status_0, &data);
+  
+  *status = (int) data;
 }
