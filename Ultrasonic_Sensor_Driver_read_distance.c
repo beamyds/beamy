3f9099a225543408d550fac55f3372cf4802926a
dpicard <damien.picard@3ds.com>
2017-10-05 14:46:09 +0200

    [GDE] Modification of runnable read_distance

--- Ultrasonic_Sensor_Driver_read_distance.c
+++ Ultrasonic_Sensor_Driver_read_distance.c
@@ -1,7 +1,7 @@
 #include "Fmi_Cfg.h" // Ids for <XXX>WriteToSharedObj
 #include "ASimSharedObjUtils.h" // <XXX>WriteToSharedObj
 
-void read_distance(int* distance){
+void read_distance(uint16_t* distance){
   fmi2Integer data;
   
   fmi2IntegerReadFromWriteBuffer(InputId_Ultrasonic_Sensor_Driver_read_distance_0, &data);
