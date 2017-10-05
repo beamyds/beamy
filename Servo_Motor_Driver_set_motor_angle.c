7524bc67d45c1e0d545ef629966d629023ce8cc9
dpicard <damien.picard@3ds.com>
2017-10-05 14:16:28 +0200

    [GDE] Modification of runnable set_motor_angle

--- Servo_Motor_Driver_set_motor_angle.c
+++ Servo_Motor_Driver_set_motor_angle.c
@@ -2,5 +2,5 @@
 #include "ASimSharedObjUtils.h" // <XXX>WriteToSharedObj
 
 void set_motor_angle(uint16_t angle){
-  	fmi2IntegerWriteToSharedObj(Servo_Motor_Driver_set_motor_angle_0, &angle);
+  	fmi2IntegerWriteToSharedObj(SharedId_Servo_Motor_Driver_set_motor_angle_0, &angle);
 }
