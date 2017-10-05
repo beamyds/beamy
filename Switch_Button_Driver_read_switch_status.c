486ba0c53eb3c1276d6fe700b59aece0c69a7b58
dpicard <damien.picard@3ds.com>
2017-10-05 11:17:40 +0200

    [GDE] Modification of runnable read_switch_status

--- Switch_Button_Driver_read_switch_status.c
+++ Switch_Button_Driver_read_switch_status.c
@@ -1,3 +1,10 @@
+#include "Fmi_Cfg.h" // Ids for <XXX>WriteToSharedObj
+#include "ASimSharedObjUtils.h" // <XXX>WriteToSharedObj
+
 void read_switch_status(uint8_t* status){
-	(void) status;
+  fmi2Integer data;
+  
+  fmi2IntegerReadFromWriteBuffer(InputId_Inertial_Measurement_Unit_read_accelerometer_0, &data);
+  
+  *status = (unint8_t) status;
 }
