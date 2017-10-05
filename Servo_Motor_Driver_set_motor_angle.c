4b1ba36f453c9ed21a8a7bf8e388d98b9c5a51dc
dpicard <damien.picard@3ds.com>
2017-10-05 12:13:19 +0200

    [GDE] Modification of runnable set_motor_angle

--- Servo_Motor_Driver_set_motor_angle.c
+++ Servo_Motor_Driver_set_motor_angle.c
@@ -1,3 +1,8 @@
+#include <math.h>
+#include "Fmi_Cfg.h" // Ids for <XXX>WriteToSharedObj
+#include "ASimSharedObjUtils.h" // <XXX>WriteToSharedObj
+
+
 void set_motor_angle(float angle){
     // FIXME
 	(void) angle;
