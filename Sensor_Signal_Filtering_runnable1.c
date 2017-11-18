007261a5dbea6c4e775d14da89c49d8b24125c5e
sdk <damien.picard@3ds.com>
2017-11-18 11:12:22 +0100

    [GDE] Initial commit

---
+++ Sensor_Signal_Filtering_runnable1.c
@@ -0,0 +1,38 @@
+{
+  "interface_": {
+    "operation": "SYSTEM",
+    "portSpecifications": []
+  },
+  "blocks": [
+    {
+      "name": "Abs1",
+      "type": {
+        "operation": "ABS",
+        "portSpecifications": [
+          {
+            "name": "1",
+            "direction": "IN",
+            "xRatio": 0.0,
+            "yRatio": 0.5,
+            "simuNumber": 0
+          },
+          {
+            "name": "2",
+            "direction": "OUT",
+            "xRatio": 1.0,
+            "yRatio": 0.5,
+            "simuNumber": 0
+          }
+        ]
+      },
+      "attributes": [],
+      "x": -30.0,
+      "y": -20.0,
+      "width": 60.0,
+      "height": 40.0,
+      "simuStartIndex": 0
+    }
+  ],
+  "ios": [],
+  "connections": []
+}
