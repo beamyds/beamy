a170120ea5f60abd66049781322feb53720feb8f
sdk <damien.picard@3ds.com>
2017-11-20 09:25:16 +0100

    [GDE] Modification of runnable runnable2

--- Sensor_Signal_Filtering_runnable2.c
+++ Sensor_Signal_Filtering_runnable2.c
@@ -37,9 +37,7 @@
       "width": 60.0,
       "height": 40.0,
       "simuStartIndex": 0
-    }
-  ],
-  "ios": [
+    },
     {
       "name": "read_distance",
       "type": {
@@ -55,8 +53,8 @@
         ]
       },
       "attributes": [],
-      "x": 0.0,
-      "y": 0.0,
+      "x": -909.1328,
+      "y": -523.0,
       "width": 30.0,
       "height": 20.0,
       "simuStartIndex": 0
@@ -76,8 +74,52 @@
         ]
       },
       "attributes": [],
-      "x": 0.0,
-      "y": 0.0,
+      "x": -440.1328,
+      "y": -493.0,
+      "width": 30.0,
+      "height": 20.0,
+      "simuStartIndex": 0
+    }
+  ],
+  "ios": [
+    {
+      "name": "read_distance",
+      "type": {
+        "operation": "PORT_SYS_IN",
+        "portSpecifications": [
+          {
+            "name": "1",
+            "direction": "OUT",
+            "xRatio": 1.0,
+            "yRatio": 0.5,
+            "simuNumber": 0
+          }
+        ]
+      },
+      "attributes": [],
+      "x": -909.1328,
+      "y": -523.0,
+      "width": 30.0,
+      "height": 20.0,
+      "simuStartIndex": 0
+    },
+    {
+      "name": "screen_distance",
+      "type": {
+        "operation": "PORT_SYS_OUT",
+        "portSpecifications": [
+          {
+            "name": "1",
+            "direction": "IN",
+            "xRatio": 0.0,
+            "yRatio": 0.5,
+            "simuNumber": 0
+          }
+        ]
+      },
+      "attributes": [],
+      "x": -440.1328,
+      "y": -493.0,
       "width": 30.0,
       "height": 20.0,
       "simuStartIndex": 0
