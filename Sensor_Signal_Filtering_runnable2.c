95d9e3e5bce7ab4fbb68cc723f94e6c93b599980
sdk <damien.picard@3ds.com>
2017-11-20 09:26:10 +0100

    [GDE] Modification of runnable runnable2

--- Sensor_Signal_Filtering_runnable2.c
+++ Sensor_Signal_Filtering_runnable2.c
@@ -45,6 +45,40 @@
       "width": 30.0,
       "height": 20.0,
       "simuStartIndex": 0
+    },
+    {
+      "name": "Low Pass Filter1",
+      "type": {
+        "operation": "LOW_PASS_FILTER",
+        "portSpecifications": [
+          {
+            "name": "1",
+            "direction": "IN",
+            "xRatio": 0.0,
+            "yRatio": 0.5,
+            "simuNumber": 0
+          },
+          {
+            "name": "2",
+            "direction": "OUT",
+            "xRatio": 1.0,
+            "yRatio": 0.5,
+            "simuNumber": 0
+          }
+        ]
+      },
+      "attributes": [
+        {
+          "name": "Coefficient",
+          "type": "http://www.w3.org/2001/XMLSchema#double",
+          "value": "1"
+        }
+      ],
+      "x": -691.1328,
+      "y": -529.0,
+      "width": 60.0,
+      "height": 40.0,
+      "simuStartIndex": 0
     }
   ],
   "ios": [
@@ -122,6 +156,100 @@
         "simuNumber": 0
       },
       "targetBlock": {
+        "name": "Low Pass Filter1",
+        "type": {
+          "operation": "LOW_PASS_FILTER",
+          "portSpecifications": [
+            {
+              "name": "1",
+              "direction": "IN",
+              "xRatio": 0.0,
+              "yRatio": 0.5,
+              "simuNumber": 0
+            },
+            {
+              "name": "2",
+              "direction": "OUT",
+              "xRatio": 1.0,
+              "yRatio": 0.5,
+              "simuNumber": 0
+            }
+          ]
+        },
+        "attributes": [
+          {
+            "name": "Coefficient",
+            "type": "http://www.w3.org/2001/XMLSchema#double",
+            "value": "1"
+          }
+        ],
+        "x": -691.1328,
+        "y": -529.0,
+        "width": 60.0,
+        "height": 40.0,
+        "simuStartIndex": 0
+      },
+      "targetPortSpecification": {
+        "name": "1",
+        "direction": "IN",
+        "xRatio": 0.0,
+        "yRatio": 0.5,
+        "simuNumber": 0
+      },
+      "points": [
+        -791.1328,
+        -524.0,
+        -741.1328,
+        -524.0,
+        -741.1328,
+        -509.0,
+        -691.1328,
+        -509.0
+      ]
+    },
+    {
+      "sourceBlock": {
+        "name": "Low Pass Filter1",
+        "type": {
+          "operation": "LOW_PASS_FILTER",
+          "portSpecifications": [
+            {
+              "name": "1",
+              "direction": "IN",
+              "xRatio": 0.0,
+              "yRatio": 0.5,
+              "simuNumber": 0
+            },
+            {
+              "name": "2",
+              "direction": "OUT",
+              "xRatio": 1.0,
+              "yRatio": 0.5,
+              "simuNumber": 0
+            }
+          ]
+        },
+        "attributes": [
+          {
+            "name": "Coefficient",
+            "type": "http://www.w3.org/2001/XMLSchema#double",
+            "value": "1"
+          }
+        ],
+        "x": -691.1328,
+        "y": -529.0,
+        "width": 60.0,
+        "height": 40.0,
+        "simuStartIndex": 0
+      },
+      "sourcePortSpecification": {
+        "name": "2",
+        "direction": "OUT",
+        "xRatio": 1.0,
+        "yRatio": 0.5,
+        "simuNumber": 0
+      },
+      "targetBlock": {
         "name": "screen_distance",
         "type": {
           "operation": "PORT_SYS_OUT",
@@ -150,11 +278,11 @@
         "simuNumber": 0
       },
       "points": [
-        -791.1328,
-        -524.0,
-        -673.1328,
-        -524.0,
-        -673.1328,
+        -631.1328,
+        -509.0,
+        -593.1328,
+        -509.0,
+        -593.1328,
         -507.0,
         -555.1328,
         -507.0
