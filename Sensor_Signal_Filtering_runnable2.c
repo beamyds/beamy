21a27f28523cae480299fd91af24635db8af248e
sdk <damien.picard@3ds.com>
2017-11-20 09:24:53 +0100

    [GDE] Initial commit

---
+++ Sensor_Signal_Filtering_runnable2.c
@@ -0,0 +1,87 @@
+{
+  "interface_": {
+    "operation": "SYSTEM",
+    "portSpecifications": []
+  },
+  "blocks": [
+    {
+      "name": "Low Pass Filter1",
+      "type": {
+        "operation": "LOW_PASS_FILTER",
+        "portSpecifications": [
+          {
+            "name": "1",
+            "direction": "IN",
+            "xRatio": 0.0,
+            "yRatio": 0.5,
+            "simuNumber": 0
+          },
+          {
+            "name": "2",
+            "direction": "OUT",
+            "xRatio": 1.0,
+            "yRatio": 0.5,
+            "simuNumber": 0
+          }
+        ]
+      },
+      "attributes": [
+        {
+          "name": "Coefficient",
+          "type": "http://www.w3.org/2001/XMLSchema#double",
+          "value": "1"
+        }
+      ],
+      "x": -30.0,
+      "y": -20.0,
+      "width": 60.0,
+      "height": 40.0,
+      "simuStartIndex": 0
+    }
+  ],
+  "ios": [
+    {
+      "name": "read_distance",
+      "type": {
+        "operation": "PORT_SYS_IN",
+        "portSpecifications": [
+          {
+            "name": "1",
+            "direction": "OUT",
+            "xRatio": 1.0,
+            "yRatio": 0.5,
+            "simuNumber": 0
+          }
+        ]
+      },
+      "attributes": [],
+      "x": 0.0,
+      "y": 0.0,
+      "width": 30.0,
+      "height": 20.0,
+      "simuStartIndex": 0
+    },
+    {
+      "name": "screen_distance",
+      "type": {
+        "operation": "PORT_SYS_OUT",
+        "portSpecifications": [
+          {
+            "name": "1",
+            "direction": "IN",
+            "xRatio": 0.0,
+            "yRatio": 0.5,
+            "simuNumber": 0
+          }
+        ]
+      },
+      "attributes": [],
+      "x": 0.0,
+      "y": 0.0,
+      "width": 30.0,
+      "height": 20.0,
+      "simuStartIndex": 0
+    }
+  ],
+  "connections": []
+}
