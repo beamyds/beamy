e2fb1a476737f8e0c73a55b65d72c16cc94b8a5d
sdk <damien.picard@3ds.com>
2017-11-20 09:26:46 +0100

    [GDE] Modification of runnable runnable3

--- Sensor_Signal_Filtering_runnable3.c
+++ Sensor_Signal_Filtering_runnable3.c
@@ -3,43 +3,7 @@
     "type": "SYSTEM",
     "portSpecifications": []
   },
-  "blockStates": [
-    {
-      "isInitial": true,
-      "name": "State1",
-      "type": {
-        "type": "STATE",
-        "portSpecifications": [
-          {
-            "name": "1",
-            "direction": "IN",
-            "xRatio": 0.5,
-            "yRatio": 0.0,
-            "simuNumber": 0
-          },
-          {
-            "name": "2",
-            "direction": "OUT",
-            "xRatio": 0.5,
-            "yRatio": 1.0,
-            "simuNumber": 0
-          },
-          {
-            "name": "Action",
-            "direction": "ACTION",
-            "xRatio": 1.0,
-            "yRatio": 0.5,
-            "simuNumber": 0
-          }
-        ]
-      },
-      "x": -687.0,
-      "y": -497.0,
-      "width": 80.0,
-      "height": 80.0,
-      "simuStartIndex": 0
-    }
-  ],
+  "blockStates": [],
   "blockActions": [],
   "blockTransitions": [],
   "connections": []
