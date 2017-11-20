7f9bd43375fc7a28cfbc7b7b86f63364874c6108
sdk <damien.picard@3ds.com>
2017-11-20 09:25:16 +0100

    [GDE] Modification of runnable runnable2

--- Sensor_Signal_Filtering_runnable2.c
+++ Sensor_Signal_Filtering_runnable2.c
@@ -58,27 +58,6 @@
       "width": 30.0,
       "height": 20.0,
       "simuStartIndex": 0
-    },
-    {
-      "name": "screen_distance",
-      "type": {
-        "operation": "PORT_SYS_OUT",
-        "portSpecifications": [
-          {
-            "name": "1",
-            "direction": "IN",
-            "xRatio": 0.0,
-            "yRatio": 0.5,
-            "simuNumber": 0
-          }
-        ]
-      },
-      "attributes": [],
-      "x": -440.1328,
-      "y": -493.0,
-      "width": 30.0,
-      "height": 20.0,
-      "simuStartIndex": 0
     }
   ],
   "ios": [
@@ -118,8 +97,8 @@
         ]
       },
       "attributes": [],
-      "x": -440.1328,
-      "y": -493.0,
+      "x": 0.0,
+      "y": 0.0,
       "width": 30.0,
       "height": 20.0,
       "simuStartIndex": 0
